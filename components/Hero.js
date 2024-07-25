37ce575a066be0016a89f68188216f61f2a2d975
Lishia Ellis <99147857+lellis09@users.noreply.github.com>
2024-07-25 18:33:14 +0000

    made changes to hero section

--- components/Hero.js
+++ components/Hero.js
@@ -3,13 +3,24 @@ import React from 'react'
 
 const Hero = () => {
     return(
-        <main className=''>
-            <section className='text-white h-screen bg-[url("/Untitled-design.svg")] bg-cover'>
-                <div className='title-cont'>
-                    <h1 className='text-3xl font-bold relative top-6 left-12'>Welcome to Bliss Bowl</h1>
+        <main className='text-white h-screen  -z-10 bg-cover '>
+             <img className='opacity-50' src="/Untitled-design.svg"></img>
+            <section className='opacity-none w-auto h-auto border-2 border-white'>
+                {/* container styled for image */}
+                {/* <div className=''>
+                   
+                </div> */}
+                <div className='z-10 relative lg:bottom-[30rem] opacity-none'>
+                    <h1 className='text-5xl font-bold pb-4 pt-8'>Welcome to Bliss Bowl</h1>
+                    <h3 className='pb-8 text-2xl'>Where every bite is a delight</h3>
                 </div>
+            
             </section>
-                
+            {/* <div className='title-cont flex flex-col justify-center items-center z-10 md:relative 
+            lg:bottom-[30rem] md:bottom-[48rem] sm:justify-start'>
+                    <h1 className='text-5xl font-bold pb-4 pt-8'>Welcome to Bliss Bowl</h1>
+                    <h3 className='pb-8 text-2xl'>Where every bite is a delight</h3>
+                </div> */}
         </main>
     )
 }
--- components/Menu.js
+++ components/Menu.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Menu = () => {
     return (
-        <nav className="flex justify-between pr-8 pb-8">
+        <nav className="flex justify-center pr-8 pb-8">
             <div className="text-paleyellow">Logo</div>
             <ul className="flex gap-x-4 text-paleyellow">
                 <li>Home</li>
